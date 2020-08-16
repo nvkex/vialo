b3a663b0b92410f8d58434b02f8c577a587a68db
nvkex <scrbwrm@gmail.com>
2020-08-16 23:33:19 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './components/HomePage/HomePage';
+import SignUp from './components/SignUp/SignUp';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
+      <Route path="/register" exact component={SignUp} />
         <Route path="/" exact component={HomePage} />
       </Switch>
     </div>
